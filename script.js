35dfcd84fe6d2a9f45c5e52417b540b0a9347340
Tasha Wolff <tashanwolff@gmail.com>
2022-09-18 13:01:20 -0700

    Fix grid layout with and without border lines

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gridBox = document.querySelector('#gridBox');
-let squares = 16;
+let squares = 18;
 let grid = squares * squares;
 let size = 600 / squares;
-let sizeLines = (600 / squares) - 1;
+let sizeLines = (600 / squares) - 2;
+let lines = true;
 
 console.log(size);
 
@@ -10,8 +11,14 @@ function makeGrid(grid) {
 for (i=0; i < grid; i++) {
     const divSqr = document.createElement('div');
     divSqr.setAttribute('style', `background: blue;`);
-    divSqr.style.height = `${size}px`;
-    divSqr.style.width = `${size}px`;
+    if (lines) {
+        divSqr.style.height = `${sizeLines}px`;
+        divSqr.style.width = `${sizeLines}px`;
+        divSqr.style.border = "1px solid black";
+    } else if (!lines){
+        divSqr.style.height = `${size}px`;
+        divSqr.style.width = `${size}px`;
+    }
     gridBox.appendChild(divSqr);
 }}
 
