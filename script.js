b580adde3f82c7921153cd0ee79cd9f79212d4c1
Tasha Wolff <tashanwolff@gmail.com>
2022-09-18 11:27:50 -0700

    Make grid generation into a function

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ let sizeLines = (600 / squares) - 1;
 
 console.log(size);
 
+function makeGrid(grid) {
 for (i=0; i < grid; i++) {
     const divSqr = document.createElement('div');
     divSqr.setAttribute('style', `background: blue;`);
     divSqr.style.height = `${size}px`;
     divSqr.style.width = `${size}px`;
     gridBox.appendChild(divSqr);
-}
+}}
+
+makeGrid(grid);
