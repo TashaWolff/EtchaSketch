307d9695f810aabe45c8b44fd58d672df7c31d2d
Tasha Wolff <tashanwolff@gmail.com>
2022-09-22 14:13:26 -0700

    Make line button into a checkbox

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 const gridBox = document.querySelector('#gridBox');
 const slider = document.getElementById("gridRange");
 const output = document.getElementById("value");
-
-let lines = true;
+const cb = document.querySelector('#lineBtn');
 
 function makeGrid(squares) {
 let grid = squares * squares;
 let size = 600 / squares;
 let sizeLines = (600 / squares) - 2;
-//can make function for for loop - genSquares(); 
+//can make function for the for loop - genSquares(); 
 for (i=0; i < grid; i++) {
     const divSqr = document.createElement('div');
+    const cb = document.querySelector('#lineBtn');
     divSqr.id = 'sqr';
     divSqr.className = 'sqr';
-    if (lines) {
+    if (!cb.checked) {
         divSqr.style.height = `${sizeLines}px`;
         divSqr.style.width = `${sizeLines}px`;
         divSqr.style.border = "1px solid black";
-    } else if (!lines){
+    } else if (cb.checked){
         divSqr.style.height = `${size}px`;
         divSqr.style.width = `${size}px`;
     }
     gridBox.appendChild(divSqr);
-    // divSqr.addEventListener('click', () => {
-    //     alert('Hello!');
-    // });
 }
 }
 
-const btnClassic = document.querySelector('#classic');
-
-btnClassic.addEventListener('click', () => {
-    alert('Hello..');
-});
-
+makeGrid(16);
 
 //slider bar
-
 output.textContent = `${slider.value}`;
 
 slider.oninput = function () {
@@ -54,7 +45,41 @@ function removeGrid() {
     }
   }
 
-makeGrid(16);
+const chosenColor = 'black';
+
+const sqrs = document.querySelectorAll('#sqr');
+
+sqrs.forEach((sqr) => {
+    sqr.addEventListener('mouseover', () => {
+        sqr.style.background = `${chosenColor}`;
+        //might remove after line checkbox
+        // sqr.style.border = `1px solid ${chosenColor}`;
+    });
+});
+
+
+//classic button
+const btnClassic = document.querySelector('#classic');
+
+btnClassic.addEventListener('click', () => {
+    alert('Hello..');
+});
+
+
+
+
+//line button
+//must be made so if clicked again, the sqaures go back
+
+// const bntLines = document.querySelector('#lineBtn');
+
+// bntLines.addEventListener('click', () => {
+//     //resetGrid()
+//     sqrs.forEach((sqr) => {
+//         sqr.style.border = `1px solid #e7dbcb`;
+//         });
+// });
+
 
 // warningDiv = document.getElementById('warning');
 
@@ -65,11 +90,3 @@ makeGrid(16);
 // warningDiv.addEventListener('mouseover', () => {
 //     warningDiv.style.background = 'black';
 // });
-
-const sqrs = document.querySelectorAll('#sqr');
-
-sqrs.forEach((sqr) => {
-    sqr.addEventListener('mouseover', () => {
-        sqr.style.background = 'black';
-    });
-});
